a45182501a34e547159acbf2cb51ddba5c214653
Odelia <odeliasusanto@gmail.com>
2018-08-03 12:55:33 +0700

    hash admin

--- seeders/20180803052110-add-data-for-login-user-admin.js
+++ seeders/20180803052110-add-data-for-login-user-admin.js
@@ -6,25 +6,25 @@ module.exports = {
      first_name: "Wisnu",
      last_name: "Gautama",
      email: "wisnu@mail.com",
-     password: "1234",
+     password: "$2b$08$sC3xPzzTAjtxDRB1CKNt7.OffWgcLZS1pyMwWxH3v6zpiVb0Gm9j.",
      role: "admin"
    }, {
     first_name: "Odie",
     last_name: "Susanto",
     email: "odie@mail.com",
-    password: "1234",
+    password: "$2b$08$sC3xPzzTAjtxDRB1CKNt7.OffWgcLZS1pyMwWxH3v6zpiVb0Gm9j.",
     role: "admin"
    }, {
     first_name: "Andre",
     last_name: "Sudi",
     email: "andre@mail.com",
-    password: "abcd",
+    password: "$2b$08$sC3xPzzTAjtxDRB1CKNt7.OffWgcLZS1pyMwWxH3v6zpiVb0Gm9j.",
     role: "user"
    }, {
     first_name: "Fajar",
     last_name: "TC",
     email: "fajar@mail.com",
-    password: "abcd",
+    password: "$2b$08$sC3xPzzTAjtxDRB1CKNt7.OffWgcLZS1pyMwWxH3v6zpiVb0Gm9j.",
     role: "user"
    }
    ], {});
